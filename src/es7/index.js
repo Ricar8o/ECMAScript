f544ac2879dafe5aaef4d1cc8294e596a2eeddb4
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2020-10-31 16:44:53 -0500

    Clase 5

---
+++ src/es7/index.js
@@ -0,0 +1,14 @@
+// CLASE 1
+// Includes
+let numbers = [1,2,3,6,8];
+
+if (numbers.includes(6)){
+    console.log('Si se encuentra el valor.');
+}else{
+    console.log('No se encuentra edl valor.')
+}
+// Exponentes
+let base = 4;
+let exponente = 3;
+let resultado = base ** exponente;
+console.log(resultado);
