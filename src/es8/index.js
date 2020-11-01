fd58b4caf19b954a76768d1eaa170c7060cf80fd
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2020-10-31 23:09:26 -0500

    Clase 7

--- src/es8/index.js
+++ src/es8/index.js
@@ -26,3 +26,30 @@ console.log(string.padStart(7,'hi'))
 console.log(string.padEnd(12,' ------'))
 console.log('food'.padEnd(12,' ------'))
 
+// CLASE 7
+
+const helloWorld = () => {
+    return new Promise((resolve,reject) => {
+        (true)
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error){
+        console.log(error);
+    }
+};
+
+anotherFunction();
