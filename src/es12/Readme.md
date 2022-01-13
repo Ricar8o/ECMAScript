f14dd02d21ce1477243e723651e876bf8a0962b1
Ricardo <ricardo.8md@gmail.com>
2022-01-12 21:16:08 -0500

    ES12

---
+++ src/es12/Readme.md
@@ -0,0 +1,23 @@
+# ECMASCRIPT 12
+
+## ReplaceAll 
+
+- Replace solo reemplazaba la primera coincidencia que encontraba, 
+- ReplaceAll reemplaza todas las ocurrencias encontradas.
+
+## Métodos privados 
+
+- Ahora podemos crear métodos privados dentro de las clases.
+- Una vez privado solo se puede ejecutar dentro de la clase. 
+
+## PromiseAny 
+
+- Es un objeto global de promise que recibe un arreglo de promesas que captura la primera que sea resuelta de manera satisfactoria.
+
+## WeakRef
+
+- Un objeto WeakRef permite mantener una referencia débil a otro objeto, sin impedir que ese objeto sea recogido por el garbage-collected de javascript.
+
+## Nuevos Operadores lógicos
+
+(Operadores Lógicos)[https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Expressions_and_Operators]
---
+++ src/es12/index.js
@@ -0,0 +1,51 @@
+// 🧯 Replace
+
+const string = "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web.";
+const replacedString = string.replace("JavaScript", "Python");
+console.log(replacedString);
+
+const replacedString2 = string.replaceAll("JavaScript", "Python");
+console.log(replacedString2);
+
+// 🔒 Metodos privados
+class Message {
+    #show(val){ // Con el # convertimos al método en privado
+        console.log(val);
+    };
+};
+
+const message = new Message();
+message.show('Hola!');
+
+//  👽 Promise Any
+
+const promise1 = new Promise((resolve, reject) => reject("1"));
+const promise2 = new Promise((resolve, reject) => resolve("2"));
+const promise3 = new Promise((resolve, reject) => resolve("3"));
+
+Promise.any([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+
+//  🦴 WeakRef(element);
+
+class AnyClass {
+    constructor(element){
+        this.ref = new WeakRef(element)
+    }
+    // code
+}
+
+// 🧪 Nuevos operadores lógicos
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue &&= isFalse); // x && (x = y)
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue ||= isFalse); // x || (x = y)
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue &&= isFalse); // x ?? (x = y)
